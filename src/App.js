5a8e376c3823992f2a1ad57b7f3d6f7008f8c61f
stopod <gjkv8@icloud.com>
2023-03-19 17:42:33 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useCallback, useMemo } from "react";
+import { ChildArea } from "./ChildArea";
 
-function App() {
+/**
+ * memo -> componentのメモ化
+ * useCallback -> 関数のメモ化
+ * useMemo -> 変数のメモ化
+ */
+
+export const App = () => {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+  const onClickOpen = () => setOpen(!open);
+
+  // 毎回新しい関数を生成している判断になる
+  // 第二引数が見張る値, 空だと最初だけ
+  const onClickClose = useCallback(() => setOpen(false), []);
+
+  // 変数のメモ化
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      {/* 新しい関数が渡される = propsが変更されている */}
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
-}
-
-export default App;
+};
